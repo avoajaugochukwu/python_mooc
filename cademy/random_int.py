db3010e984be5cc1e2a4ff48ab01847a5779b311
avoajaugochukwu <avoajaugochukwu@github.com>
2015-04-05 19:43:01 +0100

    Scrabble

--- cademy/random_int.py
+++ cademy/random_int.py
@@ -8,16 +8,11 @@ import datetime
 # print datetime.datetime.today().day
 
 
-def mainOrderId():
-	orderId = ''
-	orderId += str(datetime.datetime.today().year)
-	orderId = orderId[2:]
-	orderId += '0'
-	orderId += str(datetime.datetime.today().month)
-	orderId += str(datetime.datetime.today().day)
+def checkSex(word):
+	cut = word[:1]
+	if cut == 'M':
+		return 'M'
+	else:
+		return 'F'
 
-	for i in range(0, 4):
-		orderId += str(randint(0, 9))
-
-	orderId = 'T' + orderId
-	return orderId
+print checkSex('FeMoney')
